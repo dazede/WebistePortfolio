5ce1ae3d6b215a69b0845232f816245ebcad5f57
dazede <daisygrace@hotmail.co.nz>
2024-11-15 14:16:06 +1300

    Add new typefont and fix some styling

--- WebPortfolio/JS/app.js
+++ WebPortfolio/JS/app.js
@@ -5,6 +5,7 @@ menuCenter.onclick = function(){
     menu.classList.toggle('active')
 }
 
+/*animate menu 'petal' on click*/
 const listItems = document.querySelectorAll('.petal');
 listItems.forEach(item => {
   item.addEventListener('click', () => {
@@ -12,8 +13,24 @@ listItems.forEach(item => {
   });
 });
 
+/*delay menu navigation for anim*/
 function onMenuNavigation(url){
     setTimeout(function() {
         window.location.href = url;
     }, 1000);
+}
+
+/*store menu open state for session*/
+menuCenter.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  const isOpen = menu.classList.contains('open');
+  sessionStorage.setItem('menuState', isOpen ? 'open' : 'closed');
+});
+
+if (sessionStorage.getItem('menuState') === 'open') {
+  menu.classList.add('open');
+  setTimeout(function() {
+    menu.classList.toggle('active')
+  }, 100);
+  
 }
