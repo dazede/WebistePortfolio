ee95867f045d11663aa9eb29b51f74bd2c3459d4
dazede <daisygrace@hotmail.co.nz>
2024-11-27 15:31:16 +1300

    fix scrolling text to be infinite
    fix scrolling text to be infinite

--- WebPortfolio/JS/skill.js
+++ WebPortfolio/JS/skill.js
@@ -4,3 +4,8 @@ document.querySelectorAll('.skill-petal').forEach((element) => {
     const delay = item.getPropertyValue('--i') /4;
     element.style.animationDelay = `${delay}s`;
   });
+
+  /* clone scrolling text */
+  const text = document.getElementById('.scrolling-text');
+  const clonedText = text.cloneNode(true);
+  container.appendChild(clonedText);
