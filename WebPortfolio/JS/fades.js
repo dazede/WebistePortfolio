90c08c47265150c14a7ae76db0b9547d234f6893
dazede <daisygrace@hotmail.co.nz>
2024-11-27 14:21:39 +1300

    refine about page
    scrolls and fade ins on about page. sections off each section

--- WebPortfolio/JS/fades.js
+++ WebPortfolio/JS/fades.js
@@ -1,12 +1,23 @@
 /* about page */
+const sections = document.querySelectorAll('section');
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-        if(entry.isIntersecting){
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            currentSectionIndex = Array.from(entries).indexOf(entry);
             entry.target.classList.add('show');
+            entry.target.classList.remove('hidden');
         } else {
             entry.target.classList.remove('show');
+            entry.target.classList.add('hidden');
         }
-    })
+    });
 });
-const hiddenElements = document.querySelectorAll('.about-section')
-hiddenElements.forEach((el) => observer.observe(el))
+sections.forEach(section => observer.observe(section));
+
+const button = document.querySelector('.down-arrow');
+let currentSectionIndex = 0;
+button.addEventListener('click', () => {
+    currentSectionIndex = (currentSectionIndex + 1) % sections.length;
+    sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
+    button.style.transform = currentSectionIndex == sections.length - 1 ? "rotate(90deg)" : "rotate(-90deg)" ;
+});
