4cef5489f722ad2fcbd497038c3695ed8d063604
dazede <daisygrace@hotmail.co.nz>
2025-01-07 11:17:36 +1300

    populate about sections
    populate about page with data. fix scroll button

--- WebPortfolio/JS/fades.js
+++ WebPortfolio/JS/fades.js
@@ -3,7 +3,7 @@ const sections = document.querySelectorAll('section');
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            currentSectionIndex = Array.from(entries).indexOf(entry);
+            /*currentSectionIndex = Array.from(entries).indexOf(entry);*/
             entry.target.classList.add('show');
             entry.target.classList.remove('hidden');
         } else {
@@ -16,8 +16,13 @@ sections.forEach(section => observer.observe(section));
 
 const button = document.querySelector('.down-arrow');
 let currentSectionIndex = 0;
+console.log(sections.length);
 button.addEventListener('click', () => {
-    currentSectionIndex = (currentSectionIndex + 1) % sections.length;
+    console.log(currentSectionIndex);
+    currentSectionIndex = currentSectionIndex + 1;
+    currentSectionIndex = currentSectionIndex == sections.length ? 0 : currentSectionIndex;
+    console.log(currentSectionIndex);
     sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth' });
-    button.style.transform = currentSectionIndex == sections.length - 1 ? "rotate(90deg)" : "rotate(-90deg)" ;
+    button.style.transform = currentSectionIndex == sections.length - 1 ? "rotate(90deg)" : "rotate(-90deg)";
+    console.log("done");
 });
